1da61b97232e0717856fd02e4ac75ee38190c428
unknown <BCK@.en.boehme-weihs.de>
2009-04-17 14:04:39 +0200

    initialer commit

---
+++ src/main/scala/de/javenue/LayoutFormat.scala
@@ -0,0 +1,11 @@
+package de.javenue
+
+/**
+ * All supported layouts for charts.
+ */
+object LayoutFormat extends Enumeration {
+  type LayoutFormat = Value
+
+  // XY-Layout
+  val XY_LAYOUT = Value("XY-Layout")
+}
