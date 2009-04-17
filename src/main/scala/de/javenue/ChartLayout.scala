3d743efae23571277d98f1e7e330f0d626afc04f
unknown <BCK@.en.boehme-weihs.de>
2009-04-17 16:41:21 +0200

    initialer commit

--- src/main/scala/de/javenue/ChartLayout.scala
+++ src/main/scala/de/javenue/ChartLayout.scala
@@ -26,7 +26,13 @@ class ChartLayout(val format: GraphicsFormat.Value, val xml: Elem, val outputPat
 /*
  * TODO Description
  */
-private case class XYLayout(xml: Elem) {
+abstract class Layout
+
+case class XYLayout(xml: Elem) extends Layout {
+
+  val x_offset = 50
+  val y_offset = 50
+
   val x_size = (xml \ "x-size").text.toInt
   val y_size = (xml \ "y-size").text.toInt
   val background_color = (xml \ "background-color").text
@@ -34,16 +40,34 @@ private case class XYLayout(xml: Elem) {
   val image = new BufferedImage(x_size, y_size, BufferedImage.TYPE_INT_ARGB)
   val g2d = image.createGraphics
 
-  val r = Integer.valueOf("ff", 16).intValue
-  val g = Integer.valueOf("66", 16).intValue
-  val b = Integer.valueOf("00", 16).intValue
+  //val r = Integer.valueOf("ff", 16).intValue
+  //val g = Integer.valueOf("66", 16).intValue
+  //val b = Integer.valueOf("00", 16).intValue
 
   def create: BufferedImage = {
-    g2d.setColor(new Color(r, g, b, 255))
+    // Background
+    g2d.setColor(new Color(238, 238, 238, 255))
     g2d.fillRect(0, 0, x_size, y_size);
-    g2d.setColor(Color.RED)
-    g2d.fillRect(0, 0, 50, 50)
+
+    // X-Scale
+    g2d.setColor(new Color(128, 128, 128, 255))
+    g2d.drawLine(x1, y1, x2, y2)
+
+    // Y-Scale
+    g2d.drawLine(x1, y1, x3, y3)
     image
   }
+
+  val x1 = (x_size - (x_size - 2 * x_offset)) / 2
+
+  val y1 = y_size - y_offset
+
+  val x2 = x_size - x_offset
+
+  val y2 = y1
+
+  val x3 = x_offset
+
+  val y3 = y_offset
 
 }
